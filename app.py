4f38893c2740fbd604a2180625dc156ca51ede5b
Darshan Patil <darshan3.patil@gmail.com>
2022-11-23 20:27:25 +0530

    how to use collabarator update README.md file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print('Hello demo')
 print('doing change after creating developer branch lets see this how branching works in git')
 print('changes done by Developer2')
+print('commit after pull request')
